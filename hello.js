ae178da1d49a584e455615197a36d3d871e72de9
Sreshta2005 <kodirekkasreshta05@gmail.com>
2024-10-18 13:08:48 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,6 @@
+const n ="Alekhya";
+var a = "31209";
+var b = "CSE";
+console.log(n);
+console.log(a);
+console.log(b);
---
+++ info.js
@@ -0,0 +1,6 @@
+const n = "sreshta";
+var a = "32896"
+var b= "CSE"
+console.log(n);
+console.log(a);
+console.log(b);
